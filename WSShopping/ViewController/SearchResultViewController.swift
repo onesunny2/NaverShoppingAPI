db9743e2710cb2f68e581c89a3bc8ce4d0149f02
onesunny2 <ippne996@gmail.com>
2025-01-15 19:20:22 +0900

    [design] 검색결과 label 레이아웃 및 속성 설정

--- WSShopping/ViewController/SearchResultViewController.swift
+++ WSShopping/ViewController/SearchResultViewController.swift
@@ -6,17 +6,79 @@
 //
 
 import UIKit
+import Alamofire
+import SnapKit
 
 class SearchResultViewController: UIViewController {
+    
+    let resultCountLabel = UILabel()
+    var filteringButtons: [UIButton] = []
+//    let collectionView = UICollectionView()
+    lazy var filterStackview = {
+        let stackview = UIStackView()
+        stackview.axis = .horizontal
+        stackview.distribution = .equalSpacing
+        
+        return stackview
+    }()
     
     var nvtitle = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .white
-        navigationItem.title = nvtitle
+
+        configHierarchy()
+        configLayout()
+        configView()
     }
 
 }
 
+// MARK: - collectionView 설정
+//extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        <#code#>
+//    }
+//    
+//    
+//}
+
+// MARK: - 레이아웃 및 속성 설정
+extension SearchResultViewController: ShoppingConfigure {
+    func configHierarchy() {
+//        collectionView.delegate = self
+//        collectionView.dataSource = self
+        
+        view.addSubview(resultCountLabel)
+//        view.addSubview(filterStackview)
+        /* for index in 0...3 {
+            filterStackview.addArrangedSubview(filteringButtons[index])
+        } */
+//        view.addSubview(collectionView)
+    }
+    
+    func configLayout() {
+        resultCountLabel.snp.makeConstraints {
+            $0.top.leading.equalTo(view.safeAreaLayoutGuide).inset(10)
+            $0.width.equalTo(UIScreen.main.bounds.size.width - 30)
+        }
+    }
+    
+    func configView() {
+        view.backgroundColor = .systemBackground
+        navigationItem.title = nvtitle
+        
+        resultCountLabel.text = "22,222,222개의 검색 결과"
+        resultCountLabel.textColor = .systemGreen
+        resultCountLabel.font = .systemFont(ofSize: 15, weight: .bold)
+        resultCountLabel.numberOfLines = 1
+        resultCountLabel.textAlignment = .left
+    }
+    
+    
+}
