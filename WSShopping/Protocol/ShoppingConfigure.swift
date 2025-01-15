5151fdff1c01dec800e276efb8d767fbd325dbfd
onesunny2 <ippne996@gmail.com>
2025-01-15 16:34:57 +0900

    [design] UISearchBar 기능 제외 레이아웃 및 속성 설정

---
+++ WSShopping/Protocol/ShoppingConfigure.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingConfigure.swift
+//  WSShopping
+//
+//  Created by Lee Wonsun on 1/15/25.
+//
+
+import Foundation
+
+protocol ShoppingConfigure: AnyObject {
+    func configHierarchy()
+    func configLayout()
+    func configView()
+}
--- WSShopping/ViewController/MainViewController.swift
+++ WSShopping/ViewController/MainViewController.swift
@@ -6,13 +6,47 @@
 //
 
 import UIKit
+import SnapKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UISearchBarDelegate {
+    
+    let searchbar = UISearchBar()
+    let defaultImage = UIImageView()
+    let defaultLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .white
+        configHierarchy()
+        configLayout()
+        configView()
     }
 
+}
+
+extension MainViewController: ShoppingConfigure {
+    func configHierarchy() {
+        searchbar.delegate = self
+    
+        view.addSubview(searchbar)
+        view.addSubview(defaultImage)
+        view.addSubview(defaultLabel)
+    }
+    
+    func configLayout() {
+        searchbar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(5)
+        }
+    }
+    
+    func configView() {
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "원선's 쇼핑치링치링"
+        
+        searchbar.searchBarStyle = .minimal
+        searchbar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+    }
+    
+    
 }
