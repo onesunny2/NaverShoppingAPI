009b78121c84a265c6cfa9e7ef39850b1e86ded9
onesunny2 <ippne996@gmail.com>
2025-01-15 16:43:51 +0900

    [design] 메인VC 객체들의 레이아웃 및 속성 기본 설정 완료

--- WSShopping/ViewController/MainViewController.swift
+++ WSShopping/ViewController/MainViewController.swift
@@ -38,6 +38,18 @@ extension MainViewController: ShoppingConfigure {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(5)
         }
+        
+        defaultImage.snp.makeConstraints {
+            $0.centerY.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(60)
+            $0.height.equalTo(defaultImage.snp.width).multipliedBy(1.3)
+        }
+        
+        defaultLabel.snp.makeConstraints {
+            $0.centerX.equalTo(defaultImage)
+            $0.top.equalTo(defaultImage.snp.bottom).offset(-20)
+            $0.width.equalTo(300)
+        }
     }
     
     func configView() {
@@ -46,6 +58,15 @@ extension MainViewController: ShoppingConfigure {
         
         searchbar.searchBarStyle = .minimal
         searchbar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+        
+        defaultImage.image = UIImage(named: "shopping")
+        defaultImage.contentMode = .scaleAspectFit
+        
+        defaultLabel.text = "이것도 사고 저것도 살래 :>"
+        defaultLabel.textColor = .label
+        defaultLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+        defaultLabel.textAlignment = .center
+        defaultLabel.numberOfLines = 1
     }
     
     
