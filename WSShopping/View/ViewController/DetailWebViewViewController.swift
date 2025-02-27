83148a56281a60701465d66c57ac86991393b7a9
onesunny2 <ippne996@gmail.com>
2025-02-27 09:15:07 +0900

    [feat] WebKit 활용한 ViewController 생성

--- WSShopping/View/ViewController/DetailWebViewViewController.swift
+++ WSShopping/View/ViewController/DetailWebViewViewController.swift
@@ -6,24 +6,49 @@
 //
 
 import UIKit
+import SnapKit
+import WebKit
 
-class DetailWebViewViewController: UIViewController {
+final class DetailWebViewViewController: UIViewController, WKNavigationDelegate {
+    
+    private var webView = WKWebView()
+    private let url: String
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        webView = WKWebView(frame: view.bounds)
+        webView.navigationDelegate = self // 네비게이션 델리게이트 설정
+        configure()
+        
+        loadUrl(url)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init(url: String) {
+        self.url = url
+        super.init(nibName: nil, bundle: nil)
     }
-    */
-
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        decisionHandler(.allow)
+    }
+    
+    private func loadUrl(_ url: String) {
+        guard let detailUrl = URL(string: url) else { return }
+        
+        let request = URLRequest(url: detailUrl)
+        webView.load(request)
+    }
+    
+    private func configure() {
+        view.addSubview(webView)
+        webView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
