e96417a0a2c01d1ca04cd9ad3803d70df2851f7b
onesunny2 <ippne996@gmail.com>
2025-02-28 14:08:20 +0900

    [feat] 좋아요 여부를 저장하기 위한 HeartDefaults 생성

--- WSShopping/Manager/HeartDefaults.swift
+++ WSShopping/Manager/HeartDefaults.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+
+@propertyWrapper
+struct HeartDefaults<T> {
+    
+    let key: SavingInfo
+    let empty: T
+    
+    init(key: SavingInfo, empty: T) {
+        self.key = key
+        self.empty = empty
+    }
+    
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key.id) as? T ?? empty
+        }
+        set {
+            UserDefaults.standard.set(newValue as T, forKey: key.id)
+        }
+    }
+    
+    var projectedValue: Observable<T> {
+        return UserDefaults.standard.rx.observe(
+            T.self,
+            key.id,
+            options: [.initial, .new]
+        )
+        .map { $0 ?? empty }
+    }
+}
+
+// 외부에서 호출해서 사용 할 푸링디폴트의 key 값들
+enum SavingInfo {
+    case 좋아요(id: String)
+    
+    var id: String {
+        switch self {
+        case let .좋아요(id): return id
+        }
+    }
+
+    static func reset() {
+        for key in UserDefaults.standard.dictionaryRepresentation().keys {
+            UserDefaults.standard.removeObject(forKey: key.description)
+        }
+    }
+}
+
