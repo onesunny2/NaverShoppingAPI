104d5176243e3e43d455d4dc5f3ca17d71e2aa5c
onesunny2 <ippne996@gmail.com>
2025-03-05 01:37:23 +0900

    [feat] CRUD에 맞게 BaseRealm protocol 수정 후 RealmManager 생성

--- WSShopping/Protocol/BaseRealm.swift
+++ WSShopping/Protocol/BaseRealm.swift
@@ -8,9 +8,10 @@
 import Foundation
 import RealmSwift
 
-protocol BaseRealm {
+protocol BaseRealm: AnyObject {
+    func create<T: Object>(_ object: T)
     func read<T: Object>(_ object: T.Type) -> Results<T>
-    func write<T: Object>(_ object: T)
     func delete<T: Object>(_ object: T)
+    func update<T: Object>(_ object: T, completion: @escaping ((T) -> ()))
     func sort<T: Object>(_ object: T.Type, keyPath: String, ascending: Bool) -> Results<T>
 }
