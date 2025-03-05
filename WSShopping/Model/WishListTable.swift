0baf2d67d7e5ede83cff2092232bb323f021250b
onesunny2 <ippne996@gmail.com>
2025-03-05 17:32:31 +0900

    [feat] 위시 폴더와 위시리스트의 Realm 모델 데이터 생성

---
+++ WSShopping/Model/WishListTable.swift
@@ -0,0 +1,26 @@
+//
+//  WishListTable.swift
+//  WSShopping
+//
+//  Created by Lee Wonsun on 3/5/25.
+//
+
+import Foundation
+import RealmSwift
+
+final class WishListTable: Object {
+    
+    @Persisted var id: ObjectId
+    @Persisted var wishName: String
+    @Persisted var regDate: Date
+    
+    @Persisted(originProperty: "content") var folder: LinkingObjects<WishFolderTable>
+    
+    convenience init(id: ObjectId, wishName: String, regDate: Date) {
+        self.init()
+        
+        self.id = id
+        self.wishName = wishName
+        self.regDate = regDate
+    }
+}
---
+++ WSShopping/View/ViewController/WishFolderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WishFolderViewController.swift
+//  WSShopping
+//
+//  Created by Lee Wonsun on 3/5/25.
+//
+
+import UIKit
+
+class WishFolderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
